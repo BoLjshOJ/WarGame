960b48366ca51626490efe53deedb17d99aec5c1
boljshoj <123qwepioneeraZd>
2019-10-29 18:08:16 +0300

    init commit

---
+++ src/test/java/com/boljshoj/WarGame/WarGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boljshoj.WarGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
